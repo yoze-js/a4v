9a2685c61692e8a5536c29a77b7ff93580c762f3
Mikasa33 <mikasa33@qq.com>
2024-05-23 17:20:18 +0800

    📦 添加一些 vite 插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,50 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import unocss from 'unocss/vite'
+import Vue from '@vitejs/plugin-vue'
+import VueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import VueRouter from 'unplugin-vue-router/vite'
+import Layouts from 'vite-plugin-vue-layouts'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    VueRouter({
+      dts: 'types/typed-router.d.ts',
+      exclude: ['**/components/**'],
+    }),
 
-    unocss(),
+    // Vue must be placed after VueRouter()
+    Vue(),
+
+    VueJsx(),
+
+    VueDevTools(),
+
+    Layouts(),
+
+    Unocss(),
+
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+        'pinia',
+        VueRouterAutoImports,
+        {
+          'vue-router/auto': ['useLink'],
+        },
+      ],
+      dirs: [
+        'src/composables/**',
+        'src/stores/**',
+        'src/utils/**',
+      ],
+    }),
+
+    Components(),
   ],
 })
