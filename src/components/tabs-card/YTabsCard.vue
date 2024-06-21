a56c016c67d2832d1b00bb1d746888503c9b675d
Mikasa33 <mikasa33@qq.com>
2024-06-21 15:34:35 +0800

    🎉 新增标签卡片组件

---
+++ src/components/tabs-card/YTabsCard.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { PageCardProps } from './types'
+
+defineOptions({
+  name: 'YTabsCard',
+})
+
+defineProps<PageCardProps>()
+
+const value = defineModel<any>('value')
+</script>
+
+<template>
+  <NCard
+    :bordered="false"
+    content-class="!p-0"
+    class="wh-full"
+  >
+    <NTabs
+      v-model:value="value"
+      type="line"
+      size="large"
+      :tabs-padding="24"
+      pane-class="!px-24px !py-20px"
+      class="wh-full"
+    >
+      <NTabPane
+        v-for="item in list"
+        :key="item.name"
+        display-directive="show:lazy"
+        v-bind="item"
+        class="wh-full"
+      >
+        <slot :name="`tab-${item.name}`" />
+      </NTabPane>
+    </NTabs>
+  </NCard>
+</template>
---
+++ src/components/tabs-card/types.ts
@@ -0,0 +1,10 @@
+export interface PageCardProps {
+  /**
+   * 受控模式下的值
+   */
+  value?: any
+  /**
+   * 标签列表
+   */
+  list?: any[]
+}
